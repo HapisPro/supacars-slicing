5870c72bcc8c4bb34a72db2003f0ccf0ac588a65
alkausarhapis <alkausarhapis@gmail.com>
2024-08-23 21:48:38 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3775F1",
+        secondary: "#1A1C21",
+        tertiary: "2F3137",
+      },
+    },
+  },
+  plugins: [],
+};
